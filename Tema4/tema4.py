03510b6b9c1c5b57eb9d0ef82d7f691b3b979722
bogdanf21 <bogdanfomin21@gmail.com>
2023-03-05 20:39:40 +0200

    minor changes

--- Tema4/tema4.py
+++ Tema4/tema4.py
@@ -1,5 +1,8 @@
+import copy
 import sys
 from collections import defaultdict
+
+import numpy as np
 
 
 class SparseMatrix:
@@ -22,6 +25,8 @@ def read_sparse_matrix(filename):
         data, rows, cols = [], [], []
         for line in f:
             parts = [li.strip() for li in line.split(",") if len(li) != 0 and li != '\n']
+            if not len(parts):
+                continue
             x = float(parts[0].strip())
             i = int(parts[1].strip())
             j = int(parts[2].strip())
@@ -49,14 +54,82 @@ def read_b(filename):
     return b
 
 
-if __name__ == "__main__":
-    # 1
-    n, data, rows, cols = read_sparse_matrix("a_1.txt")
-    matrix = SparseMatrix(n, data, rows, cols)
-    free_terms = read_b("b_1.txt")
+def gauss_seidel(A, b, x0, tol=1e-6, max_iter=1000):
+    pass
+    # # TODO: Change this
+    # """
+    # Solves the linear system Ax = b using the Gauss-Seidel method.
+    #
+    # Parameters
+    # ----------
+    # A : numpy.ndarray
+    #     The coefficient matrix of the linear system.
+    # b : numpy.ndarray
+    #     The right-hand side vector of the linear system.
+    # x0 : numpy.ndarray
+    #     The initial guess for the solution vector.
+    # tol : float, optional
+    #     The tolerance for the residual norm. Default is 1e-6.
+    # max_iter : int, optional
+    #     The maximum number of iterations. Default is 1000.
+    #
+    # Returns
+    # -------
+    # numpy.ndarray
+    #     The solution vector x.
+    #
+    # Raises
+    # ------
+    # ValueError
+    #     If the coefficient matrix is not square, or if it is not diagonally dominant.
+    #
+    # """
+    # # Check that the matrix is square
+    # n, m = A.shape
+    # if n != m:
+    #     raise ValueError("Matrix A must be square")
+    #
+    # # Check that the matrix is diagonally dominant
+    # if not np.all(np.abs(A.diagonal()) >= np.sum(np.abs(A), axis=1) - np.abs(A.diagonal())):
+    #     raise ValueError("Matrix A must be diagonally dominant")
+    #
+    # # Initialize the solution vector
+    # x = x0.copy()
+    #
+    # # Perform the iterations
+    # for k in range(max_iter):
+    #     # Compute the new estimates for each unknown
+    #     for i in range(n):
+    #         x[i] = (b[i] - np.dot(A[i, :i], x[:i]) - np.dot(A[i, i + 1:], x[i + 1:])) / A[i, i]
+    #
+    #     # Check the convergence criterion
+    #     r = b - np.dot(A, x)
+    #     if np.linalg.norm(r) < tol:
+    #         break
+    #
+    # # Return the solution vector
+    # return x
 
-    if check_main_diagonal(matrix) is False:
-        print("An item on the diagonal is 0")
-        sys.exit()
+
+if __name__ == "__main__":
+    matrixes = []
+    bees = []
+    # 1
+    for i in range(1, 6):
+        n, data, rows, cols = read_sparse_matrix("a_{}.txt".format(i))
+        matrix = SparseMatrix(n, data, rows, cols)
+        free_terms = read_b("b_{}.txt".format(i))
+        matrixes.append(matrix)
+        bees.append(free_terms)
+
+    for matrix in matrixes:
+        if check_main_diagonal(matrix) is False:
+            print("An item on the diagonal is 0")
+            sys.exit()
+
+    for i, matrix in enumerate(matrixes):
+        # x_gs = gauss_seidel(matrix)
+        x_gs = copy.deepcopy(bees[i])
+        # print(f"Norm for matrix {i}", np.linalg.norm(np.dot(matrix.matrix, x_gs) - bees[i]))
 
 
